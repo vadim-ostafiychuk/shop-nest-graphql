1268838052a3735145af59b9d545f68152f9b42b
Vadim Ostafiychuk <vadim.ostafiychuk@outlook.com>
2022-07-29 12:30:51 +0300

    init project

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      debug: true,
+      playground: false,
+      plugins: [
+        ...(process.env.NODE_ENV === 'development'
+          ? [ApolloServerPluginLandingPageLocalDefault]
+          : []),
+      ],
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
